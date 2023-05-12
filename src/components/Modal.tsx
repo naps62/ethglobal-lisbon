cbdc652d43fd9679d7070136593a70de6190f850
Casey <leptone55@gmail.com>
2023-05-12 19:57:03 +0100

    spinner

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
 import { RxCross2 } from 'react-icons/rx';
+import RingLoader from 'react-spinners/RingLoader';
 export default function Modal({ close }: { close: () => void }) {
+  const [simulating, setSimulating] = useState(false);
+
   return (
     <div
       className="w-screen h-screen absolute bg-slate-400 bg-opacity-70 p-20 border-2"
@@ -12,10 +16,23 @@ export default function Modal({ close }: { close: () => void }) {
         <div className="flex justify-end absolute right-4 top-4">
           <RxCross2 className="text-3xl" onClick={close} />
         </div>
-        <div className="flex justify-around items-center h-full">
-          <button className="bg-green-500 p-4 rounded-xl">Simulate</button>
-          <button className="bg-red-500 p-4 rounded-xl">Cancel</button>
-        </div>
+        {!simulating ? (
+          <div className="flex justify-around items-center h-full">
+            <button
+              className="bg-green-500 p-4 rounded-xl"
+              onClick={() => setSimulating(true)}
+            >
+              Simulate
+            </button>
+            <button className="bg-red-500 p-4 rounded-xl" onClick={close}>
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <div className="flex justify-around items-center h-full">
+            <RingLoader color="#000" size={190} />
+          </div>
+        )}
       </div>
     </div>
   );
