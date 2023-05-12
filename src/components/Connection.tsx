25013bc1674ec04412e250d60343c2a4bdc68a6c
Casey <leptone55@gmail.com>
2023-05-12 17:43:28 +0100

    components

---
+++ src/components/Connection.tsx
@@ -0,0 +1,8 @@
+export default function Connection() {
+  return (
+    <div className="flex items-center">
+      <div className="w-2 h-2 bg-green-500 rounded-full mr-2" />
+      <div>connected</div>
+    </div>
+  );
+}
---
+++ src/components/Network.tsx
@@ -0,0 +1,9 @@
+import { BsChevronDown } from 'react-icons/bs';
+export default function Network() {
+  return (
+    <div className="bg-slate-500 rounded-xl px-4 flex items-center">
+      <div>Mainnet</div>
+      <BsChevronDown />
+    </div>
+  );
+}
