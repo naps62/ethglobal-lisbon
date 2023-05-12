bc0884b6aef867033ddd1c36077aa2c1f13a7505
Yuehan Duan <yuehanduan@yahoo.com>
2023-05-12 20:06:40 +0100

    Refactor connection indicator

--- src/components/Connection.tsx
+++ src/components/Connection.tsx
@@ -1,8 +1,13 @@
-export default function Connection() {
+export default function Connection({ isConnected = false }) {
+  const green = "bg-green-500";
+  const red = "bg-red-500";
+  console.log("isConnected", isConnected);
   return (
     <div className="flex items-center">
-      <div className="w-2 h-2 bg-green-500 rounded-full mr-2" />
-      <div>connected</div>
+      <div
+        className={`w-2 h-2 ${isConnected ? green : red} rounded-full mr-2`}
+      />
+      <div>{isConnected ? "connected" : "not connected"}</div>
     </div>
   );
 }
