6f712bcbf0a7dec1491ad9f1c4919a358b90bc8f
Casey <leptone55@gmail.com>
2023-05-12 17:50:09 +0100

    Account

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
+import Account from '@/components/Account';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <main>
       <Header />
-      {/* ACCOUNT */}
+      <Account />
       {/* DETAILS */}
     </main>
   );
