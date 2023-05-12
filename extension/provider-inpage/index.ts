1129f1e79c6e8b7cbd039e789a0afd80401cc229
Miguel Palhas <mpalhas@gmail.com>
2023-05-12 22:28:56 +0100

    code review

---
+++ extension/provider-inpage/index.ts
@@ -0,0 +1,15 @@
+import { BaseProvider } from "./BaseProvider";
+import { IronInpageProvider } from "./IronInpageProvider";
+import { StreamProvider } from "./StreamProvider";
+import {
+  initializeProvider,
+  setGlobalProvider,
+} from "./initializeInpageProvider";
+
+export {
+  BaseProvider,
+  initializeProvider,
+  IronInpageProvider,
+  setGlobalProvider,
+  StreamProvider,
+};
