e36fe0a6bfe05488fc7cce7a9473f7dae17c10a5
Yuehan Duan <yuehanduan@yahoo.com>
2023-05-12 19:01:06 +0100

    Add wagmiwrapper to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { WagmiWrapper } from "@/components/WagmiWrapper";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiWrapper>
+      <Component {...pageProps} />
+    </WagmiWrapper>
+  );
 }
