cab3a56216490094bb837e15ed93d83a7ab348b1
Casey <leptone55@gmail.com>
2023-05-12 19:10:38 +0100

    functional modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,27 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Header from "@/components/Header";
-import Account from "@/components/Account";
-import Details from "@/components/Details";
-import Balance from "@/components/Balance";
-import Modal from "@/components/Modal";
-import { WagmiWrapper } from "@/components/WagmiWrapper";
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Header from '@/components/Header';
+import Account from '@/components/Account';
+import Details from '@/components/Details';
+import Balance from '@/components/Balance';
+import Modal from '@/components/Modal';
+import { useState } from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <main>
-      <Modal />
+      {modalOpen && <Modal close={() => setModalOpen(false)} />}
       <Header />
       <div className="flex justify-center">
-        <button className="bg-red-500 p-4 rounded-xl">Modal</button>
+        <button
+          onClick={() => setModalOpen(true)}
+          className="bg-red-500 p-4 rounded-xl"
+        >
+          Modal
+        </button>
       </div>
       <Account />
       <Balance />
