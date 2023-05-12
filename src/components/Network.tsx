a171f0286fc85a466a4c9ee75af1975215007b16
Casey <leptone55@gmail.com>
2023-05-12 17:45:10 +0100

    status

--- src/components/Network.tsx
+++ src/components/Network.tsx
@@ -2,7 +2,7 @@ import { BsChevronDown } from 'react-icons/bs';
 export default function Network() {
   return (
     <div className="bg-slate-500 rounded-xl px-4 flex items-center">
-      <div>Mainnet</div>
+      <div className="pr-2">Mainnet</div>
       <BsChevronDown />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-    // className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      {/* HEADER */}
+    <main>
       <Header />
       {/* ACCOUNT */}
       {/* DETAILS */}
