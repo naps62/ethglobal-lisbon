eddf953ae4eec5afc904efee4c6d82dad2b82d85
Casey <leptone55@gmail.com>
2023-05-12 17:50:09 +0100

    Account

---
+++ src/components/Account.tsx
@@ -0,0 +1,9 @@
+import { BsThreeDots } from 'react-icons/bs';
+export default function Account() {
+  return (
+    <div className="flex justify-center items-center py-4 border-b-2">
+      <div className="pr-2">0x7546e9EA6f...22A5C</div>
+      <BsThreeDots className="text-3xl" />
+    </div>
+  );
+}
