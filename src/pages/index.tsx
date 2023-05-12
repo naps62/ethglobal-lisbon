ba08f4beffb16bc4613579984770bbb3fc94d2e6
Yuehan Duan <yuehanduan@yahoo.com>
2023-05-12 19:01:06 +0100

    Add wagmiwrapper to app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import Header from '@/components/Header';
-import Account from '@/components/Account';
-import Details from '@/components/Details';
-import Balance from '@/components/Balance';
-import Modal from '@/components/Modal';
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Account from "@/components/Account";
+import Details from "@/components/Details";
+import Balance from "@/components/Balance";
+import Modal from "@/components/Modal";
+import { WagmiWrapper } from "@/components/WagmiWrapper";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
