2c7f61a17b838fbd691fa274a10b8aad4b85e11b
Casey <leptone55@gmail.com>
2023-05-12 19:33:43 +0100

    position cross absolute

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,12 @@
 import { RxCross2 } from 'react-icons/rx';
 export default function Modal({ close }: { close: () => void }) {
   return (
-    <div className="w-screen h-screen absolute bg-slate-400 bg-opacity-70 p-20 border-2  ">
-      <div className="bg-white h-full border-2 p-10">
-        <div className="flex justify-end">
+    <div className="w-screen h-screen absolute  bg-slate-400 bg-opacity-70 p-20 border-2  ">
+      <div className="bg-white h-full border-2 p-10 relative">
+        <div className="flex justify-end absolute right-4 top-4">
           <RxCross2 className="text-3xl" onClick={close} />
         </div>
-        <div className="flex justify-around pt-44">
+        <div className="flex justify-around items-center h-full">
           <button className="bg-green-500 p-4 rounded-xl">Simulate</button>
           <button className="bg-red-500 p-4 rounded-xl">Cancel</button>
         </div>
