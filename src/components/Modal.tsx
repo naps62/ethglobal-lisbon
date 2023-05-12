2dd180e87e49fadb46092aaacf85ec512c4562f2
Casey <leptone55@gmail.com>
2023-05-12 19:10:38 +0100

    functional modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,15 @@
-export default function Modal() {
+import { RxCross2 } from 'react-icons/rx';
+export default function Modal({ close }: { close: () => void }) {
   return (
-    <div className="w-screen h-screen absolute bg-slate-400 bg-opacity-70 p-20 ">
-      <div className="bg-white h-full">
-        <button>Simulate</button>
-        <button>Cancel</button>
+    <div className="w-screen h-screen absolute bg-slate-400 bg-opacity-70 p-20 border-2 border-green-500 ">
+      <div className="bg-white h-full  border-2 border-red-500 p-10">
+        <div className="flex justify-end">
+          <RxCross2 className="text-3xl" onClick={close} />
+        </div>
+        <div className="flex ">
+          <button className="bg-green-500 p-4 rounded-xl">Simulate</button>
+          <button className="bg-red-500 p-4 rounded-xl">Cancel</button>
+        </div>
       </div>
     </div>
   );
