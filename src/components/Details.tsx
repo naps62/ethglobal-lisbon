7c69d1928ac81e2f9f01b440a7bbb6dfc1e4d65a
Casey <leptone55@gmail.com>
2023-05-12 18:47:23 +0100

    basic activity

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -14,12 +14,27 @@ export default function Details() {
       balance: 0.0,
     },
   ]);
+  const [transactions, setTransactions] = useState([
+    {
+      direction: 'send',
+      amount: 0.0,
+      date: new Date(),
+      address: '0x7546e9EA6f...22A5C',
+    },
+    {
+      direction: 'receive',
+      amount: 0.0,
+      date: new Date(),
+      address: '0x7546e9EA6f...22A5C',
+    },
+  ]);
+
   return (
     <div className="">
       <Menu active={active} setActive={setActive} />
       <div className="flex justify-center">
         {active === 'assets' && <Assets assets={assets} />}
-        {active === 'activity' && <Activity />}
+        {active === 'activity' && <Activity transactions={transactions} />}
       </div>
     </div>
   );
@@ -40,15 +55,7 @@ function Assets({ assets }: { assets: Asset[] }) {
   );
 }
 
-function Asset({
-  name,
-  logo,
-  balance,
-}: {
-  name: string;
-  logo: string;
-  balance: number;
-}) {
+function Asset({ name, logo, balance }: Asset) {
   return (
     <div className="flex border-b-2 w-full h-24 px-10 items-center justify-center">
       <div className="flex items-center justify-between w-full">
@@ -60,8 +67,25 @@ function Asset({
   );
 }
 
-function Activity() {
-  return <div>Activity</div>;
+interface Transaction {
+  direction: string;
+  amount: number;
+  date: Date;
+  address: string;
+}
+
+function Activity({ transactions }: { transactions: Transaction[] }) {
+  return (
+    <div>
+      {transactions.map((transaction) => (
+        <Transaction {...transaction} />
+      ))}
+    </div>
+  );
+}
+
+function Transaction({ direction, amount, date, address }: Transaction) {
+  return <div>hello</div>;
 }
 
 function Menu({
