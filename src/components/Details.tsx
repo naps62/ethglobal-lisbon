0dd1e5302810c6a3a5bcb5df260921d04d5b7146
Casey <leptone55@gmail.com>
2023-05-12 18:19:40 +0100

    basic menu

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,17 +1,40 @@
+import { useState } from 'react';
+
 export default function Details() {
+  const [active, setActive] = useState('assets');
   return (
     <div className="">
-      <Menu />
+      <Menu active={active} setActive={setActive} />
       <div className=""></div>
     </div>
   );
 }
 
-function Menu() {
+function Menu({
+  active,
+  setActive,
+}: {
+  active: string;
+  setActive: (s: string) => void;
+}) {
   return (
-    <div className="flex justify-around">
-      <div className="">Assets</div>
-      <div>Activity</div>
+    <div className="flex justify-around border-b-2">
+      <div
+        className={`${
+          active === 'assets' && 'text-blue-500 border-b-2 border-blue-500'
+        }`}
+        onClick={() => setActive('assets')}
+      >
+        Assets
+      </div>
+      <div
+        className={`${
+          active === 'activity' && 'text-blue-500 border-b-2 border-blue-500'
+        }`}
+        onClick={() => setActive('activity')}
+      >
+        Activity
+      </div>
     </div>
   );
 }
