7c1357cdaa8980f49e36c0ca52924a7498a1d243
Yuehan Duan <yuehanduan@yahoo.com>
2023-05-13 09:59:46 +0100

    Add impersonate account funcitonality

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useMemo } from "react";
-import { Dropdown, Button } from "@nextui-org/react";
-import { HiPlusCircle } from "react-icons/hi";
+import { Dropdown, Button, Input, useInput } from "@nextui-org/react";
 
 interface AccountsType {
   accounts: string[];
@@ -9,39 +8,65 @@ interface AccountsType {
 
 export const Accounts: React.FC<AccountsType> = ({ accounts, setAccounts }) => {
   const [selected, setSelected] = useState(new Set([accounts[0]])); // Initialize as a Set
+  const [toggleInput, setToggleInput] = useState(false);
+  const { value, reset, bindings } = useInput("");
 
   const selectedValue = useMemo(
     () => Array.from(selected).join("").replaceAll("_", " "),
     [selected]
   );
+  const onClickHandler = () => {
+    if (toggleInput) {
+      setAccounts([...accounts, value]);
+      setToggleInput(!toggleInput);
+    } else {
+      setToggleInput(!toggleInput);
+    }
+  };
+
+  // TODO: address input validation
 
   return (
     <div className="flex justify-center items-center py-4 border-b-2">
-      <Dropdown>
-        <Dropdown.Button flat color="secondary" css={{ tt: "capitalize" }}>
-          {selectedValue}
-        </Dropdown.Button>
-        <Dropdown.Menu
-          aria-label="Single selection actions"
+      {toggleInput ? (
+        <Input
+          {...bindings}
+          bordered
+          clearable
           color="primary"
-          disallowEmptySelection
-          selectionMode="single"
-          selectedKeys={selected}
-          onSelectionChange={(selection) =>
-            setSelected(selection as unknown as Set<string>)
-          }
-        >
-          {accounts.map((account: string) => {
-            return (
-              <Dropdown.Item showFullDescription={false} key={account}>
-                {account}
-              </Dropdown.Item>
-            );
-          })}
-        </Dropdown.Menu>
-      </Dropdown>
-      <Button className="ml-2" color="success" auto>
-        Add
+          labelLeft="Address"
+          placeholder="impersonate here"
+          onClearClick={reset}
+        />
+      ) : (
+        <div className="flex justify-center items-center py-4">
+          <Dropdown>
+            <Dropdown.Button flat color="secondary" css={{ tt: "capitalize" }}>
+              {selectedValue}
+            </Dropdown.Button>
+            <Dropdown.Menu
+              aria-label="Single selection actions"
+              color="primary"
+              disallowEmptySelection
+              selectionMode="single"
+              selectedKeys={selected}
+              onSelectionChange={(selection: any) =>
+                setSelected(selection as unknown as Set<string>)
+              }
+            >
+              {accounts.map((account: string) => {
+                return (
+                  <Dropdown.Item showFullDescription={false} key={account}>
+                    {account}
+                  </Dropdown.Item>
+                );
+              })}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      )}
+      <Button className="ml-2" color="success" auto onClick={onClickHandler}>
+        {toggleInput ? "Submit" : "Impersonate"}
       </Button>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { WagmiWrapper } from "@/components/WagmiWrapper";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiWrapper>
-      <Component {...pageProps} />
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
     </WagmiWrapper>
   );
 }
