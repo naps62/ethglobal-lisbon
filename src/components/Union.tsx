711f7a7b4e903e846cf6fa85858a5651d0e46109
Casey <leptone55@gmail.com>
2023-05-14 00:17:56 +0100

    format DAI

--- src/components/Union.tsx
+++ src/components/Union.tsx
@@ -1,9 +1,11 @@
+import { ethers } from "ethers";
+
 export default function Union({ union }) {
   console.log(union);
   return (
     <div className="flex border-b-2 w-full h-24 px-10 items-center justify-center">
-      <div className="flex items-center justify-between w-full">
-        <div>{union}</div>
+      <div className="flex items-center justify-center w-full">
+        <div> DAI staked: {ethers.utils.formatEther(union)}</div>
       </div>
     </div>
   );
