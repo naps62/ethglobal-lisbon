a8c85e1850d4a9c52235aabf7f9dd63fca0f84db
Casey <leptone55@gmail.com>
2023-05-14 00:14:40 +0100

    format

--- src/components/Union.tsx
+++ src/components/Union.tsx
@@ -1,8 +1,10 @@
-export default function Union({union}) {
-    console.log(union)
-    return (<div className="flex border-b-2 w-full h-24 px-10 items-center justify-center">
+export default function Union({ union }) {
+  console.log(union);
+  return (
+    <div className="flex border-b-2 w-full h-24 px-10 items-center justify-center">
       <div className="flex items-center justify-between w-full">
         <div>{union}</div>
       </div>
-    </div>);
+    </div>
+  );
 }
