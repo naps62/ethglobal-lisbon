17776f2af496eecc3659db22d3d0417acad9c986
Yuehan Duan <yuehanduan@yahoo.com>
2023-05-14 00:25:12 +0100

    Refactor menu name

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,28 +6,28 @@ export const Menu = ({
   setActive: (s: string) => void;
 }) => {
   return (
-    <div className="flex justify-around border-b-2">
+    <div className="flex justify-around border-b-2 mt-5">
       <div
         className={`${
-          active === 'activity' && 'text-blue-500 border-b-2 border-blue-500'
+          active === "activity" && "text-blue-500 border-b-2 border-blue-500"
         }`}
-        onClick={() => setActive('activity')}
+        onClick={() => setActive("activity")}
       >
         Assets
       </div>
       <div
         className={`${
-          active === 'assets' && 'text-blue-500 border-b-2 border-blue-500'
+          active === "assets" && "text-blue-500 border-b-2 border-blue-500"
         }`}
-        onClick={() => setActive('assets')}
+        onClick={() => setActive("assets")}
       >
-        Placeholder
+        Address Finder
       </div>
       <div
         className={`${
-          active === 'union' && 'text-blue-500 border-b-2 border-blue-500'
+          active === "union" && "text-blue-500 border-b-2 border-blue-500"
         }`}
-        onClick={() => setActive('union')}
+        onClick={() => setActive("union")}
       >
         Union Finance
       </div>
